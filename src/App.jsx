b6e2fed198e0303ed3cf717c6bdd7c9b138dfb9d
Bagas Fathurrahman <bagas.fathurrahman@gmail.com>
2025-10-24 10:59:46 +0700

    Create Structure

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen bg-gradient-to-br from-blue-400 via-red-400 to-pink-500 flex items-center justify-center text-5xl font-bold text-white">
+      Tailwind v3 Berhasil 🎉
+    </div>
+  );
+}
+
+export default App;
